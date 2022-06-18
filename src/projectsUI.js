2417bd473ad40b71becb8972e05cc4f9fb301814
William Zheng <williamzheng2001@gmail.com>
2022-06-18 16:33:25 -0400

    Alert duplicate project name

--- src/projectsUI.js
+++ src/projectsUI.js
@@ -1,18 +1,12 @@
 import PubSub from 'pubsub-js';
-import { ADD_PROJECT, CLICK_NEW_PROJECT } from './topics';
+import { ADD_PROJECT, CLICK_NEW_PROJECT, PROJECT_NAME_EXISTS } from './topics';
 import { projectsContainer, addProjectButton } from './dom';
 
 function init() {
     bindPlusButton();
 
     PubSub.subscribe(ADD_PROJECT, render);
-}
-
-function render(topic, project) {
-    const projectLabel = document.createElement('div');
-    projectLabel.classList.add('project');
-    projectLabel.textContent = project.name;
-    projectsContainer.appendChild(projectLabel);
+    PubSub.subscribe(PROJECT_NAME_EXISTS, alertNameExists);
 }
 
 function bindPlusButton() {
@@ -31,6 +25,17 @@ function promptProjectName() {
     } while (name === '');
 
     PubSub.publish(CLICK_NEW_PROJECT, name);
+}
+
+function render(topic, project) {
+    const projectLabel = document.createElement('div');
+    projectLabel.classList.add('project');
+    projectLabel.textContent = project.name;
+    projectsContainer.appendChild(projectLabel);
+}
+
+function alertNameExists(topic, name) {
+    alert(`A project with the name "${name}" already exists.`);
 }
 
 export { init as initProjectsUI };
