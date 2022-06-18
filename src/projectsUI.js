fbc173e69f56ce93166372dcb4524e82e138938c
William Zheng <williamzheng2001@gmail.com>
2022-06-18 15:46:29 -0400

    Publish topic when add project button is clicked

--- src/projectsUI.js
+++ src/projectsUI.js
@@ -1,9 +1,11 @@
 import PubSub from 'pubsub-js';
-import { NEW_PROJECT } from './topics';
-import { projects } from './dom';
+import { ADD_PROJECT, CLICK_NEW_PROJECT } from './topics';
+import { projects, newProjectButton } from './dom';
 
-export default function init() {
-    PubSub.subscribe(NEW_PROJECT, render);
+function init() {
+    PubSub.subscribe(ADD_PROJECT, render);
+    
+    bindPlusButton();
 }
 
 function render(topic, project) {
@@ -12,3 +14,14 @@ function render(topic, project) {
     projectLabel.textContent = project.name;
     projects.appendChild(projectLabel);
 }
+
+function bindPlusButton() {
+    newProjectButton.addEventListener('click', promptProjectName);
+}
+
+function promptProjectName() {
+    const projectName = prompt('Name:');
+    PubSub.publish(CLICK_NEW_PROJECT, projectName);
+}
+
+export { init as initProjectsUI };
