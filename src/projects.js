ff4ec217ffe6cf9f412e34e4c964ac351c285d8e
William Zheng <williamzheng2001@gmail.com>
2022-06-18 16:33:04 -0400

    Check for duplicate name

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,5 @@
 import PubSub from 'pubsub-js';
-import { ADD_PROJECT, CLICK_NEW_PROJECT } from './topics';
+import { ADD_PROJECT, CLICK_NEW_PROJECT, PROJECT_NAME_EXISTS } from './topics';
 import Project from './project';
 
 const projects = [];
@@ -17,12 +17,26 @@ function create(topic, name) {
 }
 
 function add(project) {
+    if (exists(project.name)) {
+        PubSub.publish(PROJECT_NAME_EXISTS, project.name);
+        return;
+    }
+
     projects.push(project);
     PubSub.publish(ADD_PROJECT, project);
 }
 
 function remove(index) {
     projects.splice(index, 1);
+}
+
+function exists(name) {
+    for (const project of projects) {
+        if (project.name === name) {
+            return true;
+        }
+    }
+    return false;
 }
 
 export { init as initProjects };
