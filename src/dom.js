8ae73a07bfe9f5cfdd980214f252f68c555de54b
William Zheng <williamzheng2001@gmail.com>
2022-06-21 19:26:29 -0400

    Get reference to main content elements

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,8 @@
 // Sidebar elements
 export const projectsDiv = document.querySelector('#projects');
 export const createProjectButton = document.querySelector('#sidebar #add-project');
+
+// Main content elements
+export const projectTitleDiv = document.querySelector('#title-container');
+export const todosDiv = document.querySelector('#todos');
+export const createTodoButton = document.querySelector('#add-todo');
