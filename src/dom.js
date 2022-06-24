7348fd77231ad5ccb26fc63757e8a150d35f7868
William Zheng <williamzheng2001@gmail.com>
2022-06-24 19:17:01 -0400

    Clean up HTML attributes

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,8 @@
 // Sidebar elements
 export const projectsDiv = document.querySelector('#projects');
-export const createProjectButton = document.querySelector('#add-project');
+export const createProjectButton = document.querySelector('#create-project');
 
 // Main content elements
 export const projectTitleDiv = document.querySelector('#project-title');
 export const todosDiv = document.querySelector('#todos');
-export const createTodoButton = document.querySelector('#add-todo');
+export const createTodoButton = document.querySelector('#create-todo');
