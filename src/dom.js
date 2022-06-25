ba24ca681ca90dcdc865391e491d0c8638855627
William Zheng <williamzheng2001@gmail.com>
2022-06-25 10:40:42 -0400

    Get reference to todo form inputs

--- src/dom.js
+++ src/dom.js
@@ -7,5 +7,9 @@ export const projectTitleDiv = document.querySelector('#project-title');
 export const todosDiv = document.querySelector('#todos');
 export const createTodoButton = document.querySelector('#create-todo');
 
-// Modals
+// Todo modal
 export const todoModal = document.querySelector('#todo-modal');
+export const todoNameInput = document.querySelector('#todo-name');
+export const todoDescriptionInput = document.querySelector('#todo-desc');
+export const todoDueDateInput = document.querySelector('#due-date');
+export const todoPriorityInput = document.querySelector('#priority');
