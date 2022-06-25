65e45ddd9be6a2e385b626277ad7a1008bddc30d
William Zheng <williamzheng2001@gmail.com>
2022-06-25 10:53:30 -0400

    Get reference to todo modal buttons

--- src/dom.js
+++ src/dom.js
@@ -13,3 +13,5 @@ export const todoNameInput = document.querySelector('#todo-name');
 export const todoDescriptionInput = document.querySelector('#todo-desc');
 export const todoDueDateInput = document.querySelector('#due-date');
 export const todoPriorityInput = document.querySelector('#priority');
+export const saveTodoButton = document.querySelector('#todo-modal .save');
+export const cancelTodoButton = document.querySelector('#todo-modal .cancel');
