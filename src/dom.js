0a0785669173e79c13a1320e631b6266c9e0e949
William Zheng <williamzheng2001@gmail.com>
2022-06-25 13:09:23 -0400

    Get references to todo form and submit button

--- src/dom.js
+++ src/dom.js
@@ -9,9 +9,11 @@ export const createTodoButton = document.querySelector('#create-todo');
 
 // Todo modal
 export const todoModal = document.querySelector('#todo-modal');
+export const todoForm = document.querySelector('#todo-modal form');
 export const todoNameInput = document.querySelector('#todo-name');
 export const todoDescriptionInput = document.querySelector('#todo-desc');
 export const todoDueDateInput = document.querySelector('#due-date');
 export const todoPriorityInput = document.querySelector('#priority');
+export const validateTodoFormButton = document.querySelector('#todo-modal .validate');
 export const saveTodoButton = document.querySelector('#todo-modal .save');
 export const cancelTodoButton = document.querySelector('#todo-modal .cancel');
